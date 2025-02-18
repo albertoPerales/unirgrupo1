4031cf9cece40500efa226df583e3be07fb0335d
Irene Rando <irenerandomateo@gmail.com>
2025-02-18 13:37:35 +0100

    feat(search): diseño del buscador

--- script/detail.js
+++ script/detail.js
@@ -4,11 +4,11 @@ const title = document.querySelector(".title-content");
 const info = document.querySelector(".synopsis");
 const poster = document.querySelector(".poster-content");
 const masinfo = document.querySelector(".card-body");
-const content=document.querySelector('#main-content');
+const content = document.querySelector('#main-content');
 const urlParams = new URLSearchParams(window.location.search);
 const ident = urlParams.get('id');
 //indice del vídeo a mostrar
-let indice=0;
+let indice = 0;
 
 //info de la peli
 const peli = (infopeli) => {
@@ -44,29 +44,29 @@ const peli = (infopeli) => {
     });
 };
 
-const mostrartodo=(datapeli, category)=>{
+const mostrartodo = (datapeli, category) => {
   return datapeli.map(elemento => elemento[category]).join(', ');
-  };
+};
 
 //galería de videos de la peli
-const videos=(infovideo)=>{
+const videos = (infovideo) => {
   mostrarvideo(infovideo);
   infovideo.results.forEach((element, index) => {
-    const opcion=document.createElement('li');
-    const menu=document.querySelector('.dropdown-menu');
-    opcion.innerHTML=`<a class="dropdown-item">${element.name}</a>`
+    const opcion = document.createElement('li');
+    const menu = document.querySelector('.dropdown-menu');
+    opcion.innerHTML = `<a class="dropdown-item">${element.name}</a>`
     menu.append(opcion);
-    opcion.addEventListener('click', (evento)=>{
-      indice=index;
+    opcion.addEventListener('click', (evento) => {
+      indice = index;
       mostrarvideo(infovideo);
     });
   });
 };
 
-const mostrarvideo=(infovideo)=>{
-  const videos=document.querySelector('.video');
+const mostrarvideo = (infovideo) => {
+  const videos = document.querySelector('.video');
   console.log(`funciono2 ${indice}`);
-videos.innerHTML=`<h2>Vídeos</h2>
+  videos.innerHTML = `<h2>Vídeos</h2>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/${infovideo.results[indice].key}" 
 title="YouTube video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
